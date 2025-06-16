9e439dac5a5a817aa876f6f1bf2afef1843c43a4
pfg <pfg@pfg.pw>
2025-06-15 20:38:52 -0700

    upd offsetCameraPivot

--- src/data/scripts/offsetCameraPivot.lua
+++ src/data/scripts/offsetCameraPivot.lua
@@ -2,17 +2,22 @@ local ocpConfig = {
     modelPart = nil, -- models.model.Character
     enable = true,
     allowEyeOffset = false,
-    offset = vec(0, 0, 0),
+    offset = vec(0, 27.648, 0),
 }
--- %FiguraEditor.LibSetting% ["Model Root Part", "ModelPart"] %(lib).modelPart = %(value)
--- %FiguraEditor.LibSetting% ["Offset", "vec3", [0, 0, 0]] %(lib).offset = %(value)
+-- %FiguraEditor.LibSetting% ["Body Root Part", "ModelPart"] %(lib).modelPart = %(value)
+-- %FiguraEditor.LibSetting% ["Offset", "vec3", [0, 27.648, 0]] %(lib).offset = %(value)
 -- %FiguraEditor.RenderSetting% "Disable" %(lib).enable = not %(value)
 -- %FiguraEditor.RenderSetting% "Move Crosshair (May trigger AntiCheat in multiplayer)" %(lib).enable = not %(value)
--- if we use a lua parser we can make it a bit better maybe
 
 function events.render(delta,context)
     if ocpConfig.enable and ocpConfig.modelPart then
-        local animPos = ocpConfig.modelPart:getAnimPos():add(ocpConfig.offset)
+		local animPos = ocpConfig.modelPart
+			:getPositionMatrix()
+			:translate(vec(0,0,0):sub(ocpConfig.offset))
+			:apply(ocpConfig.offset)
+		-- can't figure out how & where to apply the rotation properly
+		animPos.x = 0
+		animPos.z = 0
         renderer:setOffsetCameraPivot(animPos.x / 16, animPos.y / 16, animPos.z / 16)
         if ocpConfig.allowEyeOffset then
             renderer:setEyeOffset(animPos.x / 16, animPos.y / 16, animPos.z / 16)
