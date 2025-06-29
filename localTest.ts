5bad1fe1b2582867ee4a64a30f11fc637b8cbe95
pfg <pfg@pfg.pw>
2025-06-28 22:59:54 -0700

    .

--- localTest.ts
+++ localTest.ts
@@ -1,7 +1,7 @@
 import { generateLua, parseLua } from "@/data/generateLua";
 
 const fpath =
-	"c:\\Users\\pfg\\GameSaves\\Minecraft\\Instances\\MyWorlds\\figura\\avatars\\Nova_next\\project.figura-editor.lua";
+	"c:\\Users\\pfg\\GameSaves\\Minecraft\\Instances\\MyWorlds\\figura\\avatars\\Nova\\project.figura-editor.lua";
 const file = parseLua(await Bun.file(fpath).text());
 const res = generateLua(file);
 console.log(res.split("%__DATA_MARKER__%")[2]);
